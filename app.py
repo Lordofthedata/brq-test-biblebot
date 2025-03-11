7e21a90454329b63fc46e3cd010c7c8aacc29996
Lordofthedata <jborrack@gmx.de>
2025-03-11 16:02:17 +0100

    Update app.py
    willkommen text teil2

--- app.py
+++ app.py
@@ -52,9 +52,8 @@ with st.sidebar:
         
 # Display an initial welcome message
 if 'messages' not in st.session_state:
-    st.session_state['messages'] = [
-        {"role": "system", "content": "Willkommen bei deinem digitalen Begleiter fürs Bibelstudium. Bitte stelle mir eine Frage - ich versuche auf Basis mehrerer hundert renommierter Bibelkommentare eine Antwort auf deine Frage zu finden."}
-    ]
+    with st.chat_message("Barnabas"):
+    st.write("Willkommen bei deinem digitalen Begleiter fürs Bibelstudium 👋 Bitte stelle mir eine Frage - ich versuche auf Basis mehrerer hundert renommierter Bibelkommentare eine Antwort auf deine Frage zu finden.")
 
 # Messages in the conversation
 for message in st.session_state.messages:
